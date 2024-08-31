b857095cbc37ed98031874a1db1c52893a9e48aa
정채윤 <junghaejin79@gmail.com>
2024-08-31 11:18:07 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Container, Typography } from "@mui/material"
+function App(){
+
+  return(
+    <Container maxWidth="sm">
+      <Typography variant="h1">rdtfy</Typography>
+    </Container>
+  )
+}
+export default App
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material'
+
+const theme = createTheme
+({
+    typography:
+    {
+        h1:
+        {
+            color:"grey",
+            fontSize:"10px"
+        }
+    }
+});
+export default theme;
